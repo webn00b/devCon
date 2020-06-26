936217bfb96b53f314c5f37d9bf6f4bd20a19857
CocoGrub <fedsavin@gmail.com>
2020-06-26 13:16:41 +0300

    routes

--- server.js
+++ server.js
@@ -10,5 +10,12 @@ app.get('/', (req, res) => {
     res.send('API RUNS')
 })
 
+//DEFINE routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/post', require('./routes/api/post'))
+app.use('/api/profile', require('./routes/api/profile'))
+
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`server starts at port ${PORT} `))
