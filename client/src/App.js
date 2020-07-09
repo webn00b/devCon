d956a0354a2357b88a1738bd402b956546959f45
Fedor <fedsavin@gmail.com>
2020-07-09 20:04:50 +0300

    plus store

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,19 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Landing from './components/layouts/Landing'
 import Navbar from './components/layouts/Navbar'
 import './App.css';
-
+import { Provider } from 'react-redux';
+import store from './store'
 
 const App = () => {
     return (
+        <Provider store={store}>
         <Router>
             <>
                 <Navbar/>
                 <Route exact path='/' component={Landing}/>
             </>
         </Router>
-
+        </Provider>
 );
 }
 
