a3d974da92a53815d103e17dc0bca7651bd2fb01
CocoGrub <fedsavin@gmail.com>
2020-12-05 17:53:20 +0300

    add Readme

--- client/src/components/layouts/Spinner.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Spinner from '../../img/spin.svg'
-
-export default () => (
-    <>
-        <img src={Spinner} style={{width: '200px', margin: 'auto', display: 'block'}} alt='loading...'/>
-    </>
-)
